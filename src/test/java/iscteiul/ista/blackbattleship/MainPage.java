7b26e1273b810c33eb10217ca68bbda6b770948c
PedroAngelino <103763819+PedroAngelino@users.noreply.github.com>
2026-05-20 00:00:44 +0100

    Allure tests

--- src/test/java/iscteiul/ista/blackbattleship/MainPage.java
+++ src/test/java/iscteiul/ista/blackbattleship/MainPage.java
@@ -11,4 +11,14 @@ public class MainPage {
     public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
     public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
     public SelenideElement searchButton = $("[data-test='site-header-search-action']");
+
+
+    public SelenideElement cookieAcceptButton = $(".ch2-allow-all-btn");
+
+    public void acceptCookies() {
+
+        if (cookieAcceptButton.exists()) {
+            cookieAcceptButton.click();
+        }
+    }
 }
